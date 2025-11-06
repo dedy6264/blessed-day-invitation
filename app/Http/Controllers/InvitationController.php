c3f39a5b5a544490fe0a19d808d33650fa45b923
dedy.kusworo <dedy.kusworo@gmail.com>
2025-11-06 14:25:49 +0700

    update

--- app/Http/Controllers/InvitationController.php
+++ app/Http/Controllers/InvitationController.php
@@ -266,7 +266,7 @@ _Terima kasih banyak atas perhatiannya._\n\n
 
 Hormat kami,\n".
 $invitation->weddingEvent->couple->groom_name." & ".$invitation->weddingEvent->couple->bride_name."\n\n".
-"Invitation by ".env('APP_NAME','MAKARIOS')." Invitation\n".
+"Invitation by ".env('APP_NAME','MAKARIOS')." \n".
 "invitation.mimogo.sbs";
         // Prepare request to Fonnte API
         $data = [
@@ -589,7 +589,7 @@ _Terima kasih banyak atas perhatiannya_.\n\n
 
 Hormat kami,\n".
 $invitation->weddingEvent->couple->groom_name." & ".$invitation->weddingEvent->couple->bride_name."\n\n".
-"Invitation by ".env('APP_NAME','MAKARIOS')." Invitation\n".
+"Invitation by ".env('APP_NAME','MAKARIOS')." \n".
 "invitation.mimogo.sbs";
             // Prepare request to Fonnte API
             $delay = rand(10, 30);
